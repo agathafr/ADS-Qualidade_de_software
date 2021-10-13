b848d12721a3cb608b6bb89f66dbd9a68a860ea2
Agatha França <agathafranca@outlook.com>
2021-10-13 20:21:19 -0300

    Indenta o código

--- 29-Mar-2021/Exercicio1/src/aplicacao/Programa.java
+++ 29-Mar-2021/Exercicio1/src/aplicacao/Programa.java
@@ -1,31 +1,40 @@
 package aplicacao;
 
-import javax.swing.JOptionPane;
-
 import entidades.Moto;
 import entidades.MotoFactory;
+import javax.swing.JOptionPane;
 
 public class Programa {
-	public static void main(String[] args) {
-		MotoFactory factory = new MotoFactory();
-		while (true) {
-			try {
-				String nome = JOptionPane.showInputDialog("Informe a marca [Cancelar] para sair: ");
 
-				Moto moto = factory.create(nome);
+  public static void main(String[] args) {
+    MotoFactory factory = new MotoFactory();
+    while (true) {
+      try {
+        String nome = JOptionPane.showInputDialog(
+          "Informe a marca [Cancelar] para sair: "
+        );
 
-				exibeDados(moto);
+        Moto moto = factory.create(nome);
 
-			} catch (Exception e) {
-				break;
-			}
-		}
-		System.out.println("\nPROGRAMA FINALIZADO!");
-	}
+        exibeDados(moto);
+      } catch (Exception e) {
+        break;
+      }
+    }
+    System.out.println("\nPROGRAMA FINALIZADO!");
+  }
 
-	public static void exibeDados(Moto moto) {
-		JOptionPane.showMessageDialog(null,
-				"\nNome: " + moto.getNome() + "\nCilindrada: " + moto.getCilindrada() + "\nCor: " + moto.getCor(),
-				"Dados da moto", JOptionPane.CLOSED_OPTION);
-	}
+  public static void exibeDados(Moto moto) {
+    JOptionPane.showMessageDialog(
+      null,
+      "\nNome: " +
+      moto.getNome() +
+      "\nCilindrada: " +
+      moto.getCilindrada() +
+      "\nCor: " +
+      moto.getCor(),
+      "Dados da moto",
+      JOptionPane.CLOSED_OPTION
+    );
+  }
 }
